3b2066dcfa0c24b54497ed60336e200855d684db
James Wimmers <jimmyskydive@gmail.com>
2023-07-25 21:43:07 +1200

    Created element div container and box inside, flex box design to align elements and percentage width and padding with border box to get even boxes

---
+++ app.js
@@ -0,0 +1,10 @@
+const container = document.querySelector("body");
+const divContainer = document.createElement("div");
+divContainer.classList.add("container");
+container.appendChild(divContainer);
+
+for (let i = 0; i < 256; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square", "square" + i);
+  divContainer.appendChild(square);
+}
