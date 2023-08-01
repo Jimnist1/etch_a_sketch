38335f79a35fb58edbb66b52e0b6380335ca5cba
James Wimmers <jimmyskydive@gmail.com>
2023-08-01 22:55:25 +1200

    found error in function that continued column endlessly, fixed and now works, added flex to this style of solution and flex wrap ensures no spacing

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ container.appendChild(divContainer);
 
 function createGrid(gridSize) {
   for (let row = 0; row < gridSize; row++) {
-    for (let column = 0; column < gridSize; row++) {
+    for (let column = 0; column < gridSize; column++) {
       const square = document.createElement("div");
-      square.classList.add("square", "square" + row);
+      square.classList.add("square");
       square.style.height = 600 / gridSize + "px";
       square.style.width = 600 / gridSize + "px";
 
@@ -16,4 +16,4 @@ function createGrid(gridSize) {
   }
 }
 
-createGrid(0);
+createGrid(16);
