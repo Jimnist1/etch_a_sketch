928779016e87acf1432817f6319040def653907f
James Wimmers <jimmyskydive@gmail.com>
2023-08-01 22:31:26 +1200

    attempting to make grid more universal

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const divContainer = document.createElement("div");
 divContainer.classList.add("container");
 container.appendChild(divContainer);
 
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square", "square" + i);
-  divContainer.appendChild(square);
+function createGrid(gridSize) {
+  for (let row = 0; row < gridSize; row++) {
+    for (let column = 0; column < gridSize; row++) {
+      const square = document.createElement("div");
+      square.classList.add("square", "square" + row);
+      square.style.height = 600 / gridSize + "px";
+      square.style.width = 600 / gridSize + "px";
+
+      divContainer.appendChild(square);
+    }
+  }
 }
+
+createGrid(0);
