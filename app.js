21148ed4126cc02bb77c1e51bd3da7671319654b
James Wimmers <jimmyskydive@gmail.com>
2023-08-02 01:26:30 +1200

    completed function that adds a class to change colour, but to erase all individual changes I created a variable to hold the current size and then reused the delete grid function then create grid to render the grid to how it was prior to drawing

--- app.js
+++ app.js
@@ -4,16 +4,43 @@ divContainer.classList.add("container");
 container.appendChild(divContainer);
 
 function createGrid(gridSize) {
-  for (let row = 0; row < gridSize; row++) {
-    for (let column = 0; column < gridSize; column++) {
-      const square = document.createElement("div");
-      square.classList.add("square");
-      square.style.height = 600 / gridSize + "px";
-      square.style.width = 600 / gridSize + "px";
-
-      divContainer.appendChild(square);
+  if (gridSize > 100) alert("This Grid is dangerously large");
+  else if (gridSize < 16) alert("This Grid is just too small");
+  else {
+    resetGrid();
+    for (let row = 0; row < gridSize; row++) {
+      for (let column = 0; column < gridSize; column++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.height = 600 / gridSize + "px";
+        square.style.width = 600 / gridSize + "px";
+        square.addEventListener("mouseover", changeColour);
+        divContainer.appendChild(square);
+      }
     }
   }
 }
+function changeColour(e) {
+  e.target.classList.add("blackHover");
+}
 
+function resetGrid() {
+  while (divContainer.firstChild) {
+    divContainer.removeChild(divContainer.lastChild);
+  }
+}
 createGrid(16);
+let currentGrid = 16;
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.id == "resetButton") {
+      resetGrid();
+      createGrid(currentGrid);
+    } else {
+      let inputSize = prompt("Type the desired grid size 16 - 100");
+      currentGrid = inputSize;
+      createGrid(inputSize);
+    }
+  });
+});
