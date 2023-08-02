569d99ffa65371f77e39b8e0a847368ea14c999b
James Wimmers <jimmyskydive@gmail.com>
2023-08-02 22:32:03 +1200

    attempted to darken colours, but failed

--- app.js
+++ app.js
@@ -20,8 +20,29 @@ function createGrid(gridSize) {
     }
   }
 }
+const darken = (hexColor, magnitude) => {
+  hexColor = hexColor.replace(`#`, ``);
+  if (hexColor.length === 6) {
+    const decimalColor = parseInt(hexColor, 16);
+    let r = (decimalColor >> 16) + magnitude;
+    r > 255 && (r = 255);
+    r < 0 && (r = 0);
+    let g = (decimalColor & 0x0000ff) + magnitude;
+    g > 255 && (g = 255);
+    g < 0 && (g = 0);
+    let b = ((decimalColor >> 8) & 0x00ff) + magnitude;
+    b > 255 && (b = 255);
+    b < 0 && (b = 0);
+    return `#${(g | (b << 8) | (r << 16)).toString(16)}`;
+  } else {
+    return hexColor;
+  }
+};
 function changeColour(e) {
-  e.target.classList.add("blackHover");
+  let currentColour = e.target.style.backgroundColor;
+  if (currentColour == "") {
+    e.target.style.backgroundColor = "#808080";
+  }
 }
 
 function resetGrid() {
